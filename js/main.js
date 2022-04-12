579095e6196650aeb7f957d8c371854799465c50
Eugene <dossier1991@gmail.com>
2022-04-13 01:11:59 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+  effect: "coverflow",
+});
